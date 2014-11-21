cfbb8fdb2c1323c2811abf52759e41e39a5a16b8
dhruvsagar <dhruv.sagar@gatech.edu>
2014-11-21 18:18:07 -0500

    Harry potter movie
    A movie made using GIFS created through editing over 500 pictures in
    Python.

---
+++ actingCode.py
@@ -0,0 +1,12 @@
+#Dhruv Sagar, Melina Azzouz, Sana Ajani
+#CS1301 Section B06
+#GTiD: 903050965
+#email: dhruv.sagar@gatech.edu
+#I worked with my partners Melina and Sana, using only this semester's course materials.
+
+#this code was used for the split screen, the chase scene, and and Harry catching the snitch
+def actingCode():
+    for i in range(60):
+        forward(.1,.1)
+        wait(11)
+
